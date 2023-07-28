447620ac971583198ef417f66eff186fc9170b6f
Vincent Davis Jr <vince@underview.tech>
2023-07-28 19:31:25 -0400

    install: use vars to control installation location
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- install.sh
+++ install.sh
@@ -5,27 +5,30 @@ FAILURE=1
 
 CDIR="$(pwd)"
 
-GITURI="https://github.com"
+GIT_URL="https://github.com"
+VIM_INSTALL_DIR="${HOME}/.vim"
+VIMRC_INSTALL_DIR="${HOME}/.vimrc"
+
 
 # Create bundle + plugin directories
 create_directories() {
-	mkdir -p "${HOME}/.vim/bundle/Vundle.vim"
-	mkdir -p "${HOME}/.vim/bundle/preservim/nerdtree"
-	mkdir -p "${HOME}/.vim/bundle/airblade/vim-gitgutter"
-	mkdir -p "${HOME}/.vim/bundle/jiangmiao/auto-pairs"
-	mkdir -p "${HOME}/.vim/bundle/igankevich/mesonic"
-	mkdir -p "${HOME}/.vim/bundle/tikhomirov/vim-glsl"
-	mkdir -p "${HOME}/.vim/bundle/kergoth/vim-bitbake"
-	mkdir -p "${HOME}/.vim/bundle/bfrg/vim-cpp-modern"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/Vundle.vim"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/preservim/nerdtree"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/airblade/vim-gitgutter"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/jiangmiao/auto-pairs"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/igankevich/mesonic"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/tikhomirov/vim-glsl"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/kergoth/vim-bitbake"
+	mkdir -p "${VIM_INSTALL_DIR}/bundle/bfrg/vim-cpp-modern"
 
-	mkdir -p "${HOME}/.vim/pack/colors/start"
-	mkdir -p "${HOME}/.vim/pack/themes/start"
+	mkdir -p "${VIM_INSTALL_DIR}/pack/colors/start"
+	mkdir -p "${VIM_INSTALL_DIR}/pack/themes/start"
 
-	mkdir -p "${HOME}/.vim/syntax"
+	mkdir -p "${VIM_INSTALL_DIR}/syntax"
 
-	mkdir -p "${HOME}/.vim/autoload"
+	mkdir -p "${VIM_INSTALL_DIR}/autoload"
 
-	mkdir -p "${HOME}/.vim/colors"
+	mkdir -p "${VIM_INSTALL_DIR}/colors"
 
 	return $SUCCESS
 }
@@ -33,39 +36,42 @@ create_directories() {
 
 clone_plugins_syntax_colors() {
 	# Plugins
-	git clone "${GITURI}/VundleVim/Vundle.vim.git"   "${HOME}/.vim/bundle/Vundle.vim"             || return $FAILURE
-	git clone "${GITURI}/preservim/nerdtree.git"     "${HOME}/.vim/bundle/preservim/nerdtree"     || return $FAILURE
-	git clone "${GITURI}/airblade/vim-gitgutter.git" "${HOME}/.vim/bundle/airblade/vim-gitgutter" || return $FAILURE
-	git clone "${GITURI}/jiangmiao/auto-pairs.git"   "${HOME}/.vim/bundle/jiangmiao/auto-pairs"   || return $FAILURE
-	git clone "${GITURI}/igankevich/mesonic.git"     "${HOME}/.vim/bundle/igankevich/mesonic"     || return $FAILURE
-	git clone "${GITURI}/tikhomirov/vim-glsl.git"    "${HOME}/.vim/bundle/tikhomirov/vim-glsl"    || return $FAILURE
-	git clone "${GITURI}/kergoth/vim-bitbake.git"    "${HOME}/.vim/bundle/kergoth/vim-bitbake"    || return $FAILURE
-	git clone "${GITURI}/bfrg/vim-cpp-modern.git"    "${HOME}/.vim/bundle/bfrg/vim-cpp-modern"    || return $FAILURE
+	git clone "${GIT_URL}/VundleVim/Vundle.vim.git"   "${VIM_INSTALL_DIR}/bundle/Vundle.vim"             || return $FAILURE
+	git clone "${GIT_URL}/preservim/nerdtree.git"     "${VIM_INSTALL_DIR}/bundle/preservim/nerdtree"     || return $FAILURE
+	git clone "${GIT_URL}/airblade/vim-gitgutter.git" "${VIM_INSTALL_DIR}/bundle/airblade/vim-gitgutter" || return $FAILURE
+	git clone "${GIT_URL}/jiangmiao/auto-pairs.git"   "${VIM_INSTALL_DIR}/bundle/jiangmiao/auto-pairs"   || return $FAILURE
+	git clone "${GIT_URL}/igankevich/mesonic.git"     "${VIM_INSTALL_DIR}/bundle/igankevich/mesonic"     || return $FAILURE
+	git clone "${GIT_URL}/tikhomirov/vim-glsl.git"    "${VIM_INSTALL_DIR}/bundle/tikhomirov/vim-glsl"    || return $FAILURE
+	git clone "${GIT_URL}/kergoth/vim-bitbake.git"    "${VIM_INSTALL_DIR}/bundle/kergoth/vim-bitbake"    || return $FAILURE
+	git clone "${GIT_URL}/bfrg/vim-cpp-modern.git"    "${VIM_INSTALL_DIR}/bundle/bfrg/vim-cpp-modern"    || return $FAILURE
 
 	# Syntax
-	git clone "${GITURI}/under-view/vim-vulkan.git" "${HOME}/.vim/syntax"           || return $FAILURE
-	rm                                              "${HOME}/.vim/syntax/README.md" || return $FAILURE
+	git clone "${GIT_URL}/under-view/vim-vulkan.git" "${VIM_INSTALL_DIR}/syntax"           || return $FAILURE
+	rm                                               "${VIM_INSTALL_DIR}/syntax/README.md" || return $FAILURE
 
 	# Colors
-	git clone "${GITURI}/joshdick/onedark.vim.git"  "${HOME}/.vim/onedark.vim" || return $FAILURE
-	cp -ra    "${HOME}/.vim/onedark.vim/autoload"/* "${HOME}/.vim/autoload"    || return $FAILURE
-	cp -ra    "${HOME}/.vim/onedark.vim/colors"/*   "${HOME}/.vim/colors"      || return $FAILURE
-	rm -rf                                          "${HOME}/.vim/onedark.vim" || return $FAILURE
+	git clone "${GIT_URL}/joshdick/onedark.vim.git"       "${VIM_INSTALL_DIR}/onedark.vim" || return $FAILURE
+	cp -ra    "${VIM_INSTALL_DIR}/onedark.vim/autoload"/* "${VIM_INSTALL_DIR}/autoload"    || return $FAILURE
+	cp -ra    "${VIM_INSTALL_DIR}/onedark.vim/colors"/*   "${VIM_INSTALL_DIR}/colors"      || return $FAILURE
+	rm -rf                                                "${VIM_INSTALL_DIR}/onedark.vim" || return $FAILURE
 
 	return $SUCCESS
 }
+
 
 create_directories || {
 	echo "[x] create_directories: failed to created directories"
 	exit $FAILURE
 }
 
+
 clone_plugins_syntax_colors || {
 	echo "[x] clone_plugins_syntax_colors: failed to clone and copy plugins"
 	exit $FAILURE
 }
 
-cp -a "${CDIR}/vimrc" "${HOME}/.vimrc" || {
+
+cp -a "${CDIR}/vimrc" "${VIMRC_INSTALL_DIR}" || {
 	echo "[x] cp: failed to copy vimrc"
 	exit $FAILURE
 } 
